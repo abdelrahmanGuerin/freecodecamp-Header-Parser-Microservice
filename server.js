6699756211195280bd0c57d02bf51ffc851a887d
abdelrahman <menphiss7712@gmail.com>
2021-06-14 17:16:26 +0100

    modification

--- server.js
+++ server.js
@@ -25,8 +25,12 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
-
+app.get("/api/whoami", function (req, res) {
+  var ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+  var navigateur = req.headers['user-agent'];
+  var language = req.headers['accept-language'];
+  res.json({ipaddress: ip, language: language, software: navigateur});
+});
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
